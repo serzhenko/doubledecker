062826f907da265acebdbfe9210968c9e4d65efc
Dmitry Serzhenko <dmitry@serzhenko.me>
2023-01-02 11:21:26 +0300

    rename parameters

--- webview.py
+++ webview.py
@@ -12,21 +12,21 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route('/view/<int:id>/')
-def show_deck(id):
-    return embed_deck(id)
+@app.route('/view/<int:deck_id>/')
+def view_deck(deck_id):
+    return embed_deck(deck_id)
 
 
-@app.route('/embed/<int:id>/')
-def embed_deck(id):
-    deck = Deck.get_by_id(id)
+@app.route('/embed/<int:deck_id>/')
+def embed_deck(deck_id):
+    deck = Deck.get_by_id(deck_id)
     print(deck)
     if deck:
-        return render_template('embed.html', id=escape(id), num_pages=deck.slides_count)
+        return render_template('embed.html', id=escape(deck_id), num_pages=deck.slides_count)
     else:
         return page_not_found()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(error=''):
     return render_template('404.html'), 404
